74fc835169aa4dbff4681ea345ffe33b788c4fd2
jinnie-crafts <165927333+jinnie-crafts@users.noreply.github.com>
2026-02-06 17:00:01 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "numx-offline-v2.1";
+const CACHE_NAME = "numx-offline-v2.11";
 
 const ASSETS_TO_CACHE = [
   "./",
@@ -53,4 +53,5 @@ self.addEventListener("fetch", event => {
     caches.match(req).then(cached => cached || fetch(req))
   );
 });
+
 
